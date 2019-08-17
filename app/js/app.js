64bd2cec28692393080116cabaf3e53ed9e164c9
LucasPereira762xi <pereira762xi@gmail.com>
2019-08-17 09:07:11 -0300

    Configurando TS compiler

--- app/js/app.js
+++ app/js/app.js
@@ -1,4 +1,3 @@
 const negocio = new negociacao(new Date(), 1, 100);
-
 console.log(negocio);
 alert("oi");
--- app/js/models/negociacao.js
+++ app/js/models/negociacao.js
@@ -1,28 +1,19 @@
-class negociacao{
-
-    private _data;
-    private _quantidade;
-    private _valor;
-
-    constructor(data,quantidade , valor){
-        this._data       = data;
+class negociacao {
+    constructor(data, quantidade, valor) {
+        this._data = data;
         this._quantidade = quantidade;
-        this._valor      = valor;
+        this._valor = valor;
     }
-
-    get data(){
+    get data() {
         return this._data;
     }
-
     get quantidade() {
         return this._quantidade;
     }
-
     get valor() {
         return this._valor;
     }
-
-    get volume(){
+    get volume() {
         return this._quantidade * this._valor;
     }
 }
